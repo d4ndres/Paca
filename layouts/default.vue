a38f10ba4460dc479d6e5714bb02e6bcec50d7e6
diego rojas <d4ndres@gmail.com>
2024-04-22 18:30:38 -0500

    algunos estilos

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <header>
+  <div class="flex flex-col bg-slate-500 h-[100vh] ">
+
+    <header class="flex justify-between">
       <NuxtLink to="/dashboard">
         Dashboard
       </NuxtLink>
@@ -10,15 +11,17 @@
       </div>
       Nombre del usuario
     </header>
-    <div>
-      <aside>
+
+    <div class="flex-grow bg-gray-300 flex">
+      <aside class="w-[300px] flex flex-col">
           <NuxtLink to="/dashboard">Dashboard</NuxtLink>
           <NuxtLink to="/dashboard/labores">Labores</NuxtLink>
       </aside>
-      <main>
+      <main class="w-[100%] bg-red-50">
         <slot />
       </main>
     </div>
+
     <footer>
       footer
     </footer>
--- pages/dashboard/labores.vue
+++ pages/dashboard/labores.vue
@@ -1,15 +1,33 @@
+<script setup lang="ts">
+const route = useRoute()
+const inMain = ref(isInMain())
+watch(() => route.path, (newPath) => {
+  inMain.value = newPath === '/dashboard/labores';
+});
 
-import { NuxtPage } from '#build/components';
+function isInMain() {
+  return route.path === '/dashboard/labores' ? true : false
+}
+
+
+
+</script>
+
 <template>
   <section>
-    <article>
-      <NuxtLink to="/dashboard/labores/nuevo_registro">
-        Nuevo Registro
-      </NuxtLink>
+    
+    <article v-if="inMain">
+      <div>
+        <NuxtLink to="/dashboard/labores/nuevo_registro">
+          Nuevo Registro
+        </NuxtLink>
+      </div>
+
+      <div>
+        Lista de todos los registros
+      </div>
     </article>
-    <article>
-      Lista de todos los registros
-    </article>
-    <NuxtPage />
+
+    <NuxtPage  />
   </section>
 </template>
